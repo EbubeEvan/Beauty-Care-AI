f5a684a8f56e33ae14615a5e0ebec50ef7070976
EbubeEvan <ebubemmayie@gmail.com>
2024-06-09 10:01:32 +0100

    created file conversion functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import pdf from 'pdf-parse';
+import { Buffer } from 'buffer';
+import * as mammoth from 'mammoth';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const extractTextFromPDF = async (file: File): Promise<string> => {
+  const fileJson = JSON.stringify(file)
+  const arrayBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+  const data = await pdf(buffer);
+  return data.text;
+};
+
+export const extractTextFromDocx = async (file: File): Promise<string> => {
+  const arrayBuffer = await file.arrayBuffer();
+  const result = await mammoth.extractRawText({ arrayBuffer });
+  return result.value;
+};
