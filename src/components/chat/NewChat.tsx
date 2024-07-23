bc95007f2082187f89c823a9d7aec493a932f046
unknown <ebubemmayie@gmail.com>
2024-07-23 22:28:47 +0100

    feat: added image upload

--- src/components/chat/NewChat.tsx
+++ src/components/chat/NewChat.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useChat } from "ai/react";
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
+import Image from "next/image";
 import { Input } from "../ui/input";
 import { FlowerIcon, ImageIcon, CircleUser, Send } from "lucide-react";
 import { Card } from "../ui/card";
@@ -8,6 +10,34 @@ import { sanitizeMessage } from "@/lib/utils";
 
 export default function NewChat() {
   const { messages, input, handleInputChange, handleSubmit, error } = useChat();
+
+  const [files, setFiles] = useState<FileList | undefined>(undefined);
+  const [urls, setUrls] = useState<string[]>([]);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFiles(event.target.files);
+    }
+
+    const urlList = Array.from(event.target.files!).map((file) =>
+      URL.createObjectURL(file)
+    );
+    setUrls(urlList);
+  };
+
+  const submit = (event : FormEvent<HTMLFormElement>) => {
+    handleSubmit(event, {
+      experimental_attachments: files,
+    });
+
+    setFiles(undefined);
+    setUrls([]);
+
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+  }
 
   return (
     <div className="flex flex-col h-screen pt-10">
@@ -22,64 +52,96 @@ export default function NewChat() {
             </Card>
           </div>
           {messages.map((message) => (
-            <div
-              key={message.id}
-              className={`mb-4 flex items-start ${
-                message.role === "assistant" ? "justify-start" : "justify-end"
-              }`}
-            >
-              {message.role === "assistant" && (
-                <FlowerIcon className="h-6 w-6 ml-[-2.1rem] text-pink-500 dark:text-purple-400 min-w-24" />
-              )}
-              <Card
-                className={`bg-pink-500 px-6 py-3 text-lg font-medium text-white shadow-sm transition-colors focus:outline-none dark:bg-purple-500 border-none ${
-                  message.role === "user" ? "mr-[-1.8rem]" : "ml-[-1.8rem]"
+            <>
+              <div
+                key={message.id}
+                className={`mb-4 flex items-start ${
+                  message.role === "assistant" ? "justify-start" : "justify-end"
                 }`}
               >
-                {message.role === "assistant" ? (
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: sanitizeMessage(message.content),
-                    }}
-                  />
-                ) : (
-                  message.content
+                {message.role === "assistant" && (
+                  <FlowerIcon className="h-6 w-6 ml-[-2.1rem] text-pink-500 dark:text-purple-400 min-w-24" />
                 )}
-              </Card>
-              {message.role === "user" && (
-                <CircleUser className="h-6 w-6 mr-[-2.1rem] text-pink-500 dark:text-purple-400 min-w-24" />
-              )}
-            </div>
+                <Card
+                  className={`bg-pink-500 px-6 py-3 text-lg font-medium text-white shadow-sm transition-colors focus:outline-none dark:bg-purple-500 border-none ${
+                    message.role === "user" ? "mr-[-1.8rem]" : "ml-[-1.8rem]"
+                  }`}
+                >
+                  {message.role === "assistant" ? (
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: sanitizeMessage(message.content),
+                      }}
+                    />
+                  ) : (
+                    message.content
+                  )}
+                </Card>
+                {message.role === "user" && (
+                  <CircleUser className="h-6 w-6 mr-[-2.1rem] text-pink-500 dark:text-purple-400 min-w-24" />
+                )}
+              </div>
+              <div className="flex justify-end mb-3 gap-3">
+                {message?.experimental_attachments?.map((attachment, index) => (
+                  <Image
+                    key={`${message.id}-${index}`}
+                    src={attachment.url}
+                    width={200}
+                    height={200}
+                    alt={attachment?.name || "uploaded image"}
+                    priority={true}
+                    className="rounded-lg"
+                  />
+                ))}
+              </div>
+            </>
           ))}
         </div>
       </div>
+
       {/* Form positioned at the bottom */}
       <div className="flex justify-center">
-        <form
-          className="flex w-[90%] items-center px-5 gap-4 mb-[5rem] md:px-10 rounded-full bg-gray-200 dark:bg-gray-700 max-md:w-full"
-          onSubmit={handleSubmit}
-        >
-          <label htmlFor="image-upload" className="cursor-pointer">
-            <ImageIcon className="h-6 w-6 text-pink-500 dark:text-purple-500 overflow" />
-            <input
-              id="image-upload"
-              type="file"
-              accept="image/*"
-              className="hidden"
+        <div className="flex flex-col w-[90%] items-center py-2 mb-[5rem] px-8 md:px-10 rounded-full bg-gray-200 dark:bg-gray-700 max-md:w-full">
+          <div className="flex gap-3 -mb-1">
+            {urls.map((url, index) => (
+              <Image
+                key={index}
+                src={url}
+                alt="uploaded image"
+                width={50}
+                height={50}
+              />
+            ))}
+          </div>
+          <form
+            className="flex w-full items-center gap-4"
+            onSubmit={submit}
+          >
+            <label htmlFor="image-upload" className="cursor-pointer">
+              <ImageIcon className="h-6 w-6 text-pink-500 dark:text-purple-500 overflow" />
+              <input
+                id="image-upload"
+                type="file"
+                accept="image/*"
+                className="hidden"
+                onChange={handleImageChange}
+                multiple
+                ref={fileInputRef}
+              />
+            </label>
+            <Input
+              placeholder="Type your message..."
+              className="flex-1 rounded-lg px-4 py-8 bg-gray-200 dark:bg-gray-700 max-md:w-[80%] focus-visible:ring-transparent dark:focus-visible:ring-transparent text-lg"
+              value={input}
+              onChange={handleInputChange}
             />
-          </label>
-          <Input
-            placeholder="Type your message..."
-            className="flex-1 rounded-lg px-4 py-8 bg-gray-200 dark:bg-gray-700 max-md:w-[80%] focus-visible:ring-transparent dark:focus-visible:ring-transparent"
-            value={input}
-            onChange={handleInputChange}
-          />
-          {input && (
-            <button type="submit">
-              <Send className=" text-pink-500 hover:text-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2 dark:text-purple-500 dark:hover:text-purple-500 dark:focus:ring-purple-500" />
-            </button>
-          )}
-        </form>
+            {input && (
+              <button type="submit">
+                <Send className=" text-pink-500 hover:text-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2 dark:text-purple-500 dark:hover:text-purple-500 dark:focus:ring-purple-500" />
+              </button>
+            )}
+          </form>
+        </div>
       </div>
       {error && <p className="text-red-500 my-3">{error.message}</p>}
     </div>
