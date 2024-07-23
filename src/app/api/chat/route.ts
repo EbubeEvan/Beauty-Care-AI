da67c873dfe2fdaa0a6867b583729b5d4923c3e0
unknown <ebubemmayie@gmail.com>
2024-07-23 13:43:25 +0100

    chore: changed message submit button and increased font-size of chats

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -12,8 +12,7 @@ export async function POST(req: Request) {
       system:
         `You are a liscensed trichologist, dermatologist and cosmetologist but you don't book consultations.` +
         `You are a beauty expert with a wealth and depth of knowledge on all hair and skin types. ` +
-        `You take a holistic approach in offerring solutions to users` +
-        `You give product recommendations based on users' needs` +
+        `You take a holistic approach in offerring solutions to users and give product recommendations` +
         `You are polite and warm` +
         `You only answer beauty related prompts and politely decline other topics.`,
       messages,
--- src/components/chat/NewChat.tsx
+++ src/components/chat/NewChat.tsx
@@ -2,8 +2,7 @@
 
 import { useChat } from "ai/react";
 import { Input } from "../ui/input";
-import { Button } from "@/components/ui/button";
-import { FlowerIcon, ImageIcon, CircleUser } from "lucide-react";
+import { FlowerIcon, ImageIcon, CircleUser, Send } from "lucide-react";
 import { Card } from "../ui/card";
 import { sanitizeMessage } from "@/lib/utils";
 
@@ -15,10 +14,10 @@ export default function NewChat() {
       {/* New container */}
       <div className="flex-1 h-full overflow-y-auto flex flex-col gap-10">
         {/* Message container */}
-        <div className="flex flex-1 h-full flex-col md:pr-20 md:pl-10 gap-y-5 w-full">
+        <div className="flex flex-1 h-full flex-col md:pr-20 md:pl-10 gap-y-5 w-full max-md:overflow-x-hidden">
           <div className="mb-4 flex items-start gap-2">
             <FlowerIcon className="h-6 w-6 text-pink-500 dark:text-purple-400" />
-            <Card className="bg-pink-500 px-6 py-3 text-sm font-medium text-white shadow-sm transition-colors focus:outline-none dark:bg-purple-500 border-none">
+            <Card className="bg-pink-500 px-6 py-3 text-lg font-medium text-white shadow-sm transition-colors focus:outline-none dark:bg-purple-500 border-none">
               Hello beautiful, how may I assist you?
             </Card>
           </div>
@@ -33,7 +32,7 @@ export default function NewChat() {
                 <FlowerIcon className="h-6 w-6 ml-[-2.1rem] text-pink-500 dark:text-purple-400 min-w-24" />
               )}
               <Card
-                className={`bg-pink-500 px-6 py-3 text-sm font-medium text-white shadow-sm transition-colors focus:outline-none dark:bg-purple-500 border-none ${
+                className={`bg-pink-500 px-6 py-3 text-lg font-medium text-white shadow-sm transition-colors focus:outline-none dark:bg-purple-500 border-none ${
                   message.role === "user" ? "mr-[-1.8rem]" : "ml-[-1.8rem]"
                 }`}
               >
@@ -57,7 +56,7 @@ export default function NewChat() {
       {/* Form positioned at the bottom */}
       <div className="flex justify-center">
         <form
-          className="flex w-[90%] items-center gap-4 mb-[5rem] md:px-10 rounded-full bg-gray-200 dark:bg-gray-700 max-md:w-[20rem]"
+          className="flex w-[90%] items-center px-5 gap-4 mb-[5rem] md:px-10 rounded-full bg-gray-200 dark:bg-gray-700 max-md:w-full"
           onSubmit={handleSubmit}
         >
           <label htmlFor="image-upload" className="cursor-pointer">
@@ -71,16 +70,15 @@ export default function NewChat() {
           </label>
           <Input
             placeholder="Type your message..."
-            className="flex-1 rounded-lg px-4 py-10 bg-gray-200 dark:bg-gray-700 max-md:w-[80%] focus-visible:ring-transparent dark:focus-visible:ring-transparent"
+            className="flex-1 rounded-lg px-4 py-8 bg-gray-200 dark:bg-gray-700 max-md:w-[80%] focus-visible:ring-transparent dark:focus-visible:ring-transparent"
             value={input}
             onChange={handleInputChange}
           />
-          <Button
-            type="submit"
-            className="bg-pink-500 px-6 py-3 text-sm font-medium text-white shadow-sm hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2 dark:bg-purple-500 dark:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-500"
-          >
-            Send
-          </Button>
+          {input && (
+            <button type="submit">
+              <Send className=" text-pink-500 hover:text-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2 dark:text-purple-500 dark:hover:text-purple-500 dark:focus:ring-purple-500" />
+            </button>
+          )}
         </form>
       </div>
       {error && <p className="text-red-500 my-3">{error.message}</p>}
