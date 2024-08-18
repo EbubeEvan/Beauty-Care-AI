866bcca909e7a7fea4c09f2edeec7cf4c5e44f19
evangeline <ebubemmayie@gmail.com>
2024-08-18 17:49:43 +0100

    chore: deleted skeletoncard

--- src/components/chat/SkeletonCard.tsx
+++
@@ -1,9 +0,0 @@
-import { Skeleton } from "@/components/ui/skeleton"
-
-export function SkeletonCard() {
-  return (
-    <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-    </div>
-  )
-}
