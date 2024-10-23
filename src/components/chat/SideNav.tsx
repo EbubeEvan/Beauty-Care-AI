a8d7f89a083d96b1afbc807157b401f59f89cdd1
evangeline <ebubemmayie@gmail.com>
2024-10-23 23:38:23 +0100

    feat: made the history div scrollable and highlighted active history

--- src/components/chat/SideNav.tsx
+++ src/components/chat/SideNav.tsx
@@ -1,27 +1,32 @@
-"use client"
+"use client";
 
-import { Dispatch, SetStateAction } from "react";
 import { Menu, Plus, MessageCircle } from "lucide-react";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import clsx from "clsx";
-import { HistoryType } from "@/lib/types";
 import Logout from "./Logout";
 import { Skeleton } from "../ui/skeleton";
 import { Spinner } from "../ui/spinner";
 import { useFetchHistory } from "@/hooks/useFetchHistory";
 import useStore from "@/lib/store/useStore";
+import { usePathname } from 'next/navigation';
 
 export default function SideNav({
-  id
-}: {
-  id?: string
-}) {
-  const {data : history, isFetching, isLoading} = useFetchHistory(id!)
-  const { messageCount, menuOpen, setMenuOpen } = useStore()
+  id,
+}: Readonly<{
+  id?: string;
+}>) {
+  const { data: history, isFetching, isLoading } = useFetchHistory(id!);
+  const { messageCount, menuOpen, setMenuOpen } = useStore();
+  const pathname = usePathname()
+
+  console.log({pathname});
+  const pathID = pathname.split('/')[2]
+
+  console.log({pathID});
 
   return (
-    <div className="py-6">
+    <div className="py-6 h-full">
       <div className="flex flex-col gap-10 px-3 h-full">
         <div
           className={clsx("flex transition-all duration-300", {
@@ -50,23 +55,26 @@ export default function SideNav({
             {menuOpen && "New Chat"}
           </Link>
         </div>
+        {/* Make the history div scrollable */}
         <div
           className={clsx(
-            "flex flex-col justify-center gap-3 overflow-y-auto",
-            {
-              hidden: !menuOpen,
-            }
+            "flex flex-col gap-3 overflow-y-auto", // Enables vertical scrolling
+            { hidden: !menuOpen }
           )}
-        > 
-         
-          {isFetching && (messageCount === 2) && <Skeleton className="h-10 w-full rounded-full" />}
+          style={{ maxHeight: 'calc(100vh - 200px)' }} // Adjust the height as needed
+        >
+          {isFetching && messageCount === 2 && (
+            <Skeleton className="h-10 w-full rounded-full" />
+          )}
           {isLoading ? (
             <Spinner size="medium" />
           ) : (
             history?.map((chat) => (
               <Link
                 href={`/chat/${chat.chatId}`}
-                className="flex gap-2 text-white px-5 py-2 hover:bg-pink-300 dark:hover:bg-purple-400 rounded-full"
+                className={clsx("flex gap-2 text-white px-5 py-2 mr-3 hover:bg-pink-300 dark:hover:bg-purple-400 rounded-full", {
+                  "bg-pink-300 dark:bg-purple-400" : chat.chatId === pathID
+                })}
                 key={chat.chatId}
               >
                 <MessageCircle className="min-w-4 max-w-4" />
